cc17403c7158c13e2488d31f88db5a04542f4aa3
Pisnut <rayner.freddie@btinternet.com>
2025-06-24 16:06:34 +0100

    Install cytoscape.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState({ original: [], sorted: [] });
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/sort")
+        .then((res) => res.json())
+        .then((result) => setData(result));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{ padding: 20 }}>
+        <h1>Sorting Visualizer</h1>
+        <p><strong>Original:</strong> {data.original.join(", ")}</p>
+        <p><strong>Sorted:</strong> {data.sorted.join(", ")}</p>
+      </div>
   );
 }
 
 export default App;
+
